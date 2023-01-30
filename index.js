b9abc05b0115591646d1045ed576ec01fc44bf07
Ramez Nashaat <rameznashaat9999@gmail.com>
2023-01-30 17:57:26 +0200

    77

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const firebase = require("firebase-admin");
-const functions = require('firebase-functions');
-const firestore = require("firebase-admin").firestore;
+// const firebase = require("firebase-admin");
+// const functions = require('firebase-functions');
+// const firestore = require("firebase-admin").firestore;
 const express = require('express');
 const twilio = require('twilio');
 const app = express();
 
-var serviceAccount = require("./offline-shop-4f02a-firebase-adminsdk-ak1tp-b7077262a1.json");
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://offline-shop-4f02a-default-rtdb.firebaseio.com"
-});
+// var serviceAccount = require("./offline-shop-4f02a-firebase-adminsdk-ak1tp-b7077262a1.json");
+// firebase.initializeApp({
+  // credential: firebase.credential.cert(serviceAccount),
+  // databaseURL: "https://offline-shop-4f02a-default-rtdb.firebaseio.com"
+// });
 
-const db = firebase.firestore();
+// const db = firebase.firestore();
 
 
 const accountSid = "AC03ad19d36edfa6197c157034a26144af";
@@ -25,7 +25,6 @@ app.post('/verify-phone', (req, res) => {
           .create({ to: phoneNumber, channel: 'sms' })
           .then(verification => res.status(202).send(verification.status))
           .catch(error => res.status(404).send(phoneNumber));
-          
       });
 app.post('/verify-code', (req, res) => {
        const phoneNumber = req.body.phoneNumber; // phone number in E.164 format
